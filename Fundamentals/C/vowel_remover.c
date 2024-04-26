7c6d9623980935b020656b0e0bce2c441399c171
Prakhyath <96770035+Nerd9A@users.noreply.github.com>
2024-04-26 14:45:54 +0530

    Challenge II :  Vowel Remover
    Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
    
    Examples :
    - "hello"     -->  "hll"
    - "codewars"  -->  "cdwrs"
    - "goodbye"   -->  "gdby"
    - "HELLO"     -->  "HELLO"

--- Fundamentals/C/vowel_remover.c
+++ Fundamentals/C/vowel_remover.c
@@ -1,1 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+
+char* shortcut(char* str_out, const char* str_in, char* result);
+
+int main()
+{
+    char input[] = "codewars";
+    char output[100];
+    char result[100];
+
+    shortcut(output, input, result);
+    printf("Output: %s\n", output);
+
+    return 0;
+}
+
+char* shortcut(char* str_out, const char* str_in, char* result)
+{
+    int j = 0;
+    const char vowels[6] = "aeiou";
+
+    for (int i = 0; str_in[i] != '\0'; i++)
+    {
+        int isVowel = 0;
+
+        for (int k=0 ; vowels[k] != '\0'; k++)
+        {
+            if (str_in[i] == vowels[k])
+            {
+                isVowel = 1;
+                break;
+            }
+         
+        }
+            
+        if (!isVowel)
+        {
+            result[j] = str_in[i];
+            j++;
+        }
+       
+    }
+                
+    result[j] = '\0';
+
+    strcpy(str_out, result);
+    return str_out;
+}
